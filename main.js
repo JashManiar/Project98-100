5b195bbaf526fae76f60731ab1c177ff54171fd5
Jash Maniar <101001262+JashManiar@users.noreply.github.com>
2022-08-30 18:29:55 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,65 +1,53 @@
 var SpeechRecognition = window.webkitSpeechRecognition;
-var Content;
 var recognition = new SpeechRecognition();
 
-function start()
-{
+function start() {
+    document.getElementById("textbox").innerHTML = "";
     recognition.start();
-} 
-
-
-camera = document.getElementById("camera");
-Webcam.set({
-    width:500,
-    height:400,
-    image_format : 'png',
-    jpeg_quality:90
-});
-
-
-
-function speak(){
-
-    
-
-    var synth = window.speechSynthesis;
-    Webcam.attach(camera);
-
-    speak_data = "Taking your next Selfie in 5 seconds";
-    var utterThis = new SpeechSynthesisUtterance(speak_data);
-    synth.speak(utterThis);
-
-    setTimeout(function(){
-        img_id = "selfie1";
-        take_snapshot();
-        save();
-    },5000);
-
-    setTimeout(function(){
-        img_id = "selfie2";
-        take_snapshot();
-        save();
-    },10000);
-
-    setTimeout(function(){
-        img_id = "selfie3";
-        take_snapshot();
-        save();
-    },15000);
 }
 
-function take_snapshot()
-{
-    Webcam.snap(function(data_uri){
-        if (img_id== "selfie1"){
-            document.getElementById("result").innerHTML = "<img id='selfie_1' src='"+data_uri+"'>";    
-        }
-        if (img_id== "selfie2"){
-            document.getElementById("result").innerHTML = "<img id='selfie_2' src='"+data_uri+"'>";    
-        }
-        if (img_id== "selfie3"){
-            document.getElementById("result").innerHTML = "<img id='selfie_3' src='"+data_uri+"'>";    
-        }
+recognition.onresult = function (event) {
+    console.log(event);
+    var content = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML = content;
+    if (content == "take my selfie") {
+        speak()
+    }
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data = "taking your selfie in 5 seconds";
+    var utterthis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterthis);
+    Webcam.attach(camera);
+
+    setTimeout(function () {
+        takesnapshot();
+        save();
+    }, 5000);
+
+}
+
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+
+});
+
+camera = document.getElementById("camera");
+
+function takesnapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = "<img id='selfie_image' src='"+data_uri+"'>"
     });
 }
 
+function save() {
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click()
+}
